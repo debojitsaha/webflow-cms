b389c6be9100b11d0e29b29ceed549cd67754a44
debojitsaha <saha05debojit@gmail.com>
2025-02-22 00:01:28 +0530

    Refactor CORS middleware configuration in app.ts for improved readability

--- src/app.ts
+++ src/app.ts
@@ -17,11 +17,7 @@ import express from "express"; // express is the framework for the backend
 import swaggerUi from "swagger-ui-express"; // swagger is the package we use for better documentation of the api
 
 // Import custom packages we'll be using
-import {
-    connect,
-    getConnectionState,
-    isValidConnectionURI,
-} from "./config/db.config"; // has code to establish connection the mongo db
+import { connect, getConnectionState, isValidConnectionURI } from "./config/db.config"; // has code to establish connection the mongo db
 import { swaggerSpec } from "./config/swagger.config"; // has configuration for swagger
 import { mainRouter } from "./routes/main.route";
 import { logger } from "./utils/logger.util";
@@ -44,7 +40,11 @@ const app: express.Application = express();
 app.disable("x-powered-by");
 
 // Add some external middlewares. These middlewares will always function for every request our express app receives.
-app.use(cors()); // allows cross origin resource sharing. Edit and add whitelisted servers only
+const corsConfig = {
+    origin: "*",
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+};
+app.use(cors(corsConfig)); // allows cross origin resource sharing. Edit and add whitelisted servers only
 app.use(express.json()); // specifies that the type of json in request body and response body will be JSON
 app.use(morgan("combined")); // use morgan to log each request
 app.use(express.urlencoded({ extended: true })); // this middleware parses the incoming request body else it wouldn't be identified as a paylod data.
@@ -69,9 +69,7 @@ app.listen(PORT, async () => {
     // Logging for dev and prod environments
     if (String(process.env.NODE_ENV) !== "test") {
         // tslint:disable-next-line:no-console
-        console.log(
-            `${getConnectionState(db.connection.readyState)} to the database`
-        );
+        console.log(`${getConnectionState(db.connection.readyState)} to the database`);
 
         // tslint:disable-next-line:no-console
         console.log(`Listening on ${BASE_URL}:${PORT}...`);
