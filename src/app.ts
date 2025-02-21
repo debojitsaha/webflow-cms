046b4ab0071111e38ae807c4760a3bbcc45f77b1
debojitsaha <saha05debojit@gmail.com>
2025-02-22 00:41:17 +0530

    Add root route handler to respond with database connection state and greeting

--- src/app.ts
+++ src/app.ts
@@ -59,6 +59,11 @@ app.use(express.static(path.join(__dirname, "../static"))); // defining director
 app.get("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Add grouped routes to the express app from ./routes/main.route.ts
+app.get("/", async (req, res) => {
+    const db = await connect(MONGO_URI);
+    res.send(`${getConnectionState(db.connection.readyState)} to the database`);
+    res.send("Hello from Vercel!");
+});
 app.use("/", mainRouter);
 
 // Start the express server in the defined port, this too uses a callback function which we have written right inside.
